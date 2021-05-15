4e4ff7092511823719ed4adf522283994c413859
ahurli <anna.hurlimann@breakthroughenergy.org>
2021-05-14 18:13:49 -0700

    feat: add mermaid to docker

--- source/conf.py
+++ source/conf.py
@@ -214,3 +214,7 @@ epub_exclude_files = ["search.html"]
 
 # -- Extension configuration -------------------------------------------------
 todo_include_todos = True
+
+import errno
+import sphinx.util.osutil
+sphinx.util.osutil.ENOENT = errno.ENOENT
